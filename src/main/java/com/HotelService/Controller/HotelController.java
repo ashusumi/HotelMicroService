abea0ca59027897795d41cb58ca1815b92a145bb
unknown <ashishparte@nimapinfotech.com>
2024-01-18 10:51:20 +0530

    final hotel service

--- src/main/java/com/HotelService/Controller/HotelController.java
+++ src/main/java/com/HotelService/Controller/HotelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -62,6 +63,7 @@ public class HotelController {
 		}
 	} 
 	
+	@PreAuthorize("hasAuthority('OIDC_USER')")
 	@GetMapping("/hotelById")
 	public ResponseEntity<?> getHotelById(@RequestParam Integer id) {
 		try {
